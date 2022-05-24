f7065362fa61c5da23bd2ed9af8442663d8b2af1
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-05-24 14:04:46 -0300

    Finalizei ex008 de condição simples

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) { //Condição simples
+    console.log('Você ultrapassou o limite de velocidade permitido. MULTADO!!') 
+}
+console.log(`Diriga sempre usano o cinto de segurança`)
