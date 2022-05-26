7d18ccb982373126cb592f16bd9eda737bc306f0
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-05-26 17:46:40 -0300

    Concluído  exe014

--- ex014/script.js
+++ ex014/script.js
@@ -3,15 +3,15 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    
+
     msg.innerHTML = `Agora são ${hora} horas`
-    
+
     if (hora >= 0 && hora < 12) {
         img.src = 'midia/fotomanha.png'
         document.body.style.background = '#D0B074'
         //Bom dia
     } else if (hora >= 12 && hora < 18){
-        img.src = 'midia/fototarde.png'
+        img.src = 'midia/fototarde.png' //N esquecer do /midia caso a img não esteja na pagina do arquivo principal
         document.body.style.background = '#F47F51'
         //Boa Tarde
     } else{
