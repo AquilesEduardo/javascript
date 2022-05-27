fe91942732ff3fef2cb55c78210062b7836d49b6
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-05-27 14:38:30 -0300

    Tentei terminar o segundo exercicio sozinho

--- modelo/script.js
+++ modelo/script.js
@@ -1,3 +1,18 @@
 function verificar() {
-    alert('Funcionou')
+    var idade = document.getElementById('pesquisar')
+    var msg = document.getElementById('msg')
+    var data = new Date()
+    var datahoje = data.getFullYear()
+    var nasc = Number(idade.value)
+    var idd =  datahoje - nasc
+   msg.innerHTML = `SUa idade é ${idd}`
+    document.body.style.img = 'fotos/hveio.jpg'
+    /*if(idd <= 15){
+        msg.innerHTML = `Detectamos criança de ${idd} anos.`
+
+    } else if(idd <= 45 ){
+        msg.innerHTML = `Detectamos adulto de ${idd} anos.`
+    } else (idd > 45){
+        msg.innerHTML = `Detectamos idoso de ${idd} anos.`
+    }*/
 }
