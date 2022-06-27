fcf5c49aa8735b4d876cbb9df6aab376017df75b
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-06-27 16:17:05 -0300

    exercício resolvido

--- ex016/script.js
+++ ex016/script.js
@@ -3,14 +3,19 @@ let ini = document.getElementById('txti')
 let fim = document.getElementById('txtf')
 let passo = document.getElementById('txtp')
 let res = document.getElementById(`res`)
+
 if (ini.value.length == 0 || fim.value.length ==0 || passo.value.length == 0) {
-    window.alert(`[ERRO] Faltam dados!`)
-    res.innerHTML = `IMPOSSÌVEL CONTAR!!`
+    window.alert(`Impossível contar!`)
+     res.innerHTML = `Faltam Dados`
 } else {
-   res.innerHTML = 'Contando: '
+   res.innerHTML = 'Contando: <br> '
    let i = Number(ini.value)
    let f = Number(fim.value)
    let p = Number(passo.value)
+    if (i < f) {
+        window.alert(`Passo inválido! Considerando PASSO 1`)
+        p = 1
+    }
    //Contagem crescente
     if (i < f) {
         for (let c = i; c <= f; c += p)
