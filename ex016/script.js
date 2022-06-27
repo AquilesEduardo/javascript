b1a4f6bc14e3e8c04257ad0fb74b5384f737cc95
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-06-27 16:04:33 -0300

    Contagem quase finalizada

--- ex016/script.js
+++ ex016/script.js
@@ -1,10 +1,26 @@
-var inicio = document.getElementById('inicio')
-var fim = document.getElementById('fim')
-var passo = document.getElementById('passo')
-var dif = fim - inicio
-var cont = inicio
-
-while(dif <= fim && cont <= fim){
-    cont.innerHTML = `${cont}`
-    cont = cont + 1
+function contar() {
+let ini = document.getElementById('txti')
+let fim = document.getElementById('txtf')
+let passo = document.getElementById('txtp')
+let res = document.getElementById(`res`)
+if (ini.value.length == 0 || fim.value.length ==0 || passo.value.length == 0) {
+    window.alert(`[ERRO] Faltam dados!`)
+    res.innerHTML = `IMPOSSÌVEL CONTAR!!`
+} else {
+   res.innerHTML = 'Contando: '
+   let i = Number(ini.value)
+   let f = Number(fim.value)
+   let p = Number(passo.value)
+   //Contagem crescente
+    if (i < f) {
+        for (let c = i; c <= f; c += p)
+        res.innerHTML += `${c} \u{1f449}`
+    } else {
+        //Contagem regressiva  
+        for (let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1f449}`
+    } 
+}
+res.innerHTML += `\u{1f3c1}`
+}
 }
