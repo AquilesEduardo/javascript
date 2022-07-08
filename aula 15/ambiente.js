e0453b945feb2bb820157214f429dfdda33c5849
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-07-08 15:03:48 -0300

    Arquivo ambiente concluido
    Um arquivo de exercicio foi concluído

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,2 +1,5 @@
 let num = [5, 5, 2, 3, 7]
-console.log(`Nosso vetor é o ${num}`)
+num.sort()
+num.push(1)
+console.log(`Nosso vetor tem ${num.length} elementos ${num}`)
+console.log(`O primeiro valor é ${num[0]}`)
