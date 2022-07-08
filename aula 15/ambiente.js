6a28a0b45f1c1ab7d795057ed2d44fa84a5c4fea
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-07-08 15:37:37 -0300

    Conclusão da aula 15 sobre variáveis compostas

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,5 +1,12 @@
-let num = [5, 5, 2, 3, 7]
+let num = [5, 8, 2, 9, 3]
 num.sort()
 num.push(1)
 console.log(`Nosso vetor tem ${num.length} elementos ${num}`)
 console.log(`O primeiro valor é ${num[0]}`)
+
+let pos = num.indexOf(9)
+    if (pos == -1){
+        console.log(`O valor não foi encontrado!`)
+    } else{
+        console.log(`O valor está na posição ${pos} (ele não ta nessa posição não senhor)`) /*perceba que o 8 não vai ta na posição 3 pois tenho um NUM.SORT() logo após a declaração do vetor.*/
+    }
