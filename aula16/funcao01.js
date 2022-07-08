615705f1cf9b3d17402638b6ea0da3631e89a856
Aquiles Eduardo <104160212+AquilesEduardo@users.noreply.github.com>
2022-07-08 16:02:46 -0300

    Primeiro exercicio de funçao concluído

---
+++ aula16/funcao01.js
@@ -0,0 +1,9 @@
+ function parimpar(n){
+    if (n%2 == 0){
+        return `PAR`
+    } else {
+        return `IMPAR`
+    }
+ }
+ console.log(parimpar(223))
+ //console.log(res). Isso aqui seria para mostrar na tela o resultado da função
